05883c875a0f933aecb80357bfebec7f48af0025
Luca <luca-c@hotmail.de>
2021-03-09 18:08:27 +0100

    Updated to 1.2

--- user.js
+++ user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Soundcloud Anti Repost
-// @version      1.1
+// @version      1.2
 // @author       Redcrafter
 // @description  Remove reposts from soundcloud
 // @license      MIT
@@ -17,44 +17,52 @@ function removeReposts() {
     }
 }
 
-// Searches for the playerManager module
-function module(e, t, _require) {
-    let modules = _require.c;
+function replaceModule(key, oldModule) {
+    newModules[key] = (e, t, r) => {
+        oldModule(e, t, r);
+        let oldExp = e.exports;
 
-    for (let moduleid in modules) {
-        if (modules.hasOwnProperty(moduleid)) {
-            let el = _require(moduleid);
+        e.exports = (asdf) => {
+            let playManager = oldExp(asdf);
 
-            if (typeof el.playCurrent === 'function') {
-                init(el);
-                break;
-            }
+            let oldSet = playManager.setCurrentItem;
+            playManager.setCurrentItem = (e) => {
+                let r = false;
+
+                try {
+                    // Not sure if this safe
+                    r = e._submodels[0]._events["change:user"][1].context.options.actionType === "repost";
+                } catch (error) {
+                    // console.error("An Exception occoured while trying to determine repost status");
+                }
+
+                if (r) {
+                    playManager.removeItem(e);
+                    playManager.playNext();
+                } else {
+                    oldSet(e);
+                }
+            };
+
+            return temp;
         }
     }
 }
 
-function init(playManager) {
-    // Inject a function to check for reposts
-    let oldSet = playManager.setCurrentItem;
-    playManager.setCurrentItem = (e) => {
-        let r = false;
-        try {
-            // Not sure if this safe
-            r = e._submodels[0]._events["change:user"][1].context.options.actionType === "repost";
-        } catch (error) {
-            // console.error("An Exception occoured while trying to determine repost status");
-        }
+setInterval(removeReposts, 500);
 
-        if (r) {
-            playManager.removeItem(e);
-            playManager.playNext();
-        } else {
-            oldSet(e);
-        }
-    };
+let newModules = {};
 
-    setInterval(removeReposts, 500);
+// find player module
+for (const item of webpackChunk) {
+    for (const key in item[1]) {
+
+        // probably not safe
+        if (key === "86616") {
+            replaceModule(key, item[1][key]);
+        }
+    }
 }
 
-// injection our own module
-unsafeWindow.webpackJsonp.push([[1000000], { 1000000: module }, [[1000000]]]);
+// replace player module
+unsafeWindow.webpackChunk.push([[1234], newModules]);
